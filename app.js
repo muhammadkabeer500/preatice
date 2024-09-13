c1b2cb62350ce6e201c8399a0b02ca0ae6e36cfd
kabeersheikh <kabeersheikh008@gmail.com>
2024-09-13 19:29:38 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+let arr = [
+    [0,1,2,3,4],
+    [0,1,2,3],
+    [0,1,2]
+];
+
+for (let i = 0; i < arr.length; i++) {
+  
+    
+    for (let j = 0; j < arr[i].length; j++) {
+        console.log(`Index ${i}: ${arr[i]}`);
+    }
+}
+
+
